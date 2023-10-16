f919952e3c1690ddb398bc5d26d8404c185a962f
Willi <Wilelch25@gmail.com>
2023-10-16 12:42:35 +0200

    Slope of Arena and foundations for car rotation

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -2,6 +2,7 @@
 #include <Gosu/AutoLink.hpp>
 #include <iostream>
 #include <cmath>
+#define PI 3.14159265
 using namespace std;
 
 const int WIDTH = Gosu::screen_width();
@@ -9,7 +10,7 @@ const int HEIGHT = Gosu::screen_height();
 
 //infos: unter Rahmen: 105px;
 struct Arena {
-	double stretchFactor = (double)HEIGHT / 2 / pow((double)WIDTH);
+	double stretchFactor = (double)HEIGHT / 2 / pow((double)WIDTH / 4,2);
 	int XInSegment(int X) {//0 for left, 1 for middle, 2 for right
 		if (X < WIDTH / 4) { return 0; }
 		else if (X > WIDTH * 3 / 4) { return 2; }
@@ -18,11 +19,11 @@ struct Arena {
 	int YOfArenaCurve(double X) {
 		switch (XInSegment(X)) {
 		case 0://			105 nach oben       skalierung Parabel							verschiebung Parabel
-			return HEIGHT - (105 + (stretchFactor / 4, 2)) * pow(X - (double)WIDTH / 4, 2));
+			return HEIGHT - (105 + stretchFactor * pow(X - (double)WIDTH / 4, 2));
 		case 1:
 			return HEIGHT - 105;
 		case 2:
-			return HEIGHT - (105 + (stretchFactor / 4, 2)) * pow(X - (double)WIDTH * 3 / 4, 2));
+			return HEIGHT - (105 + stretchFactor * pow(X - (double)WIDTH * 3 / 4, 2));
 		}
 	}
 	int SlopeOfArenaCurve(double X) {
@@ -35,12 +36,18 @@ struct Arena {
 			return stretchFactor * 2 * (X - (double)WIDTH * 3 / 4);
 		}
 	}
+	int RotationOfArenaCurve(double X) {
+		return atan(this->SlopeOfArenaCurve(X)) * 180 / PI;
+	}
 	void draw() {
 		for (int x = 0; x < WIDTH; x++) {
-			for (int y = YOfArenaCurve(x); y < HEIGHT; y++) {
+			Gosu::Graphics::draw_rect(x, this->YOfArenaCurve(x), 5, 5, Gosu::Color::BLACK, 0);
+		}
+		/*for (int x = 0; x < WIDTH; x++) {
+			for (int y = this->YOfArenaCurve(x); y < HEIGHT; y++) {
 				Gosu::Graphics::draw_rect(x, y, 1, 1, Gosu::Color::BLACK, 0);
 			}
-		}
+		}*/
 	}
 };
 
@@ -48,6 +55,8 @@ struct Arena {
 class Car {
 	double velocityX = 0;
 	double velocityY = 0;
+	double velocity = 0; //gesamt Betrag
+	double gravityY = 0;
 	int positionX = 0;
 	int positionY = 0;
 	int rotation = 0; //in degree
@@ -56,13 +65,7 @@ class Car {
 	const Gosu::Image image_mirrored;
 	const int ACCELERATION = 100;
 	const int GRAVITY = 150;
-
-	bool carOverCurve() {
-		if (this->getEndY() < YOfArenaCurve(this->getEndY())) {
-			return 1;
-		}
-		else { return 0; }
-	}
+	Arena arena;
 
 	int getCenterX() {
 		return this->positionX + this->image.width() / 2;
@@ -76,9 +79,55 @@ class Car {
 	int getEndY() {
 		return this->positionY + this->image.height();
 	}
+	bool carOverCurve() {
+		if (this->getEndY() < arena.YOfArenaCurve(this->getCenterX())) {
+			return 1;
+		}
+		else { 
+			return 0; 
+		}
+	}
+	void gravity(){
+		if (this->carOverCurve()) {
+			this->gravityY += GRAVITY / 60;
+		}
+		else{
+			this->gravityY = 0;
+		}
+	}
+	void rotatedVelocity() {
+		this->rotation = arena.RotationOfArenaCurve(this->getCenterX());
+		this->velocityX = this->velocity * cos(this->rotation);
+		this->velocityY = this->velocity * sin(this->rotation);
+	}
+	void movement() {
+		static double counterX = 0;
+		static double counterY = 0;
+		counterX += this->velocityX / 60;
+		counterY += (gravityY + velocityY) / 60;
+		if ((int)counterX) {
+			this->positionX += (int)counterX;
+			counterX -= (int)counterX;
+		}
+		if ((int)counterY) {
+			this->positionY += (int)counterY;
+			counterY -= (int)counterY;
+		}
+	}
+	void isMirrored() {
+		if (this->velocityX < 0) { //wenn Auto nach rechts zeigt, aber v nach links geht, also negativ ist
+			this->mirrored = true;
+		}
+		else if (this->velocityX > 0) {
+			this->mirrored = false;
+		}
+	}
+	
+
 public:
-	Car(string fileprename, int positionX, int positionY) :
-		image(fileprename + ".png"), image_mirrored(fileprename + "_mirrored.png")
+	Car(string fileprename, Arena arena, int positionX, int positionY) :
+		image(fileprename + ".png"), image_mirrored(fileprename + "_mirrored.png"),
+		arena(arena)
 	{ 
 		this->positionX = positionX - image.width() / 2;
 		this->positionY = positionY - image.height();
@@ -98,36 +147,19 @@ public:
 		}
 	}
 
-	double counterX = 0;
-	double counterY = 0;
 	void move() {
-		counterX += this->velocityX / 60;
-		counterY += this->velocityY / 60;
-		if ((int)counterX) {
-			this->positionX += (int)counterX;
-			counterX -= (int)counterX;
-		}
-		if ((int)counterY) {
-			this->positionY += (int)counterY;
-			counterY -= (int)counterY;
-		}
-		if (this->velocityX < 0 ) { //wenn Auto nach rechts zeigt, aber v nach links geht, also negativ ist
-			this->mirrored = true;
-		}
-		else if (this->velocityX > 0) {
-			this->mirrored = false;
-		}
-		if (this->carOverCurve()) {
-			this->velocityY += GRAVITY / 60;
-		}
-		if (!this->carOverCurve() && this->velocityY > 0) {
-			this->velocityY = 0;
-		}
+		this->rotatedVelocity();
+		this->gravity();
+		this->isMirrored();
+		this->movement();
 	}
-	void accelerate(int directionFactor = 1) {
-		this->velocityX += ACCELERATION * cos(this->rotation) / 60 * directionFactor;
-		this->velocityY -= ACCELERATION * sin(this->rotation) /60 * directionFactor;
-		cout << "acc call" << this->velocityX << endl;
+	void accelerate(Gosu::Button btnLeft, Gosu::Button btnRight) {
+		if (Gosu::Input::down(btnLeft)) {
+			this->velocity += ACCELERATION / 60 * -1;
+		}
+		if (Gosu::Input::down(btnRight)) {
+			this->velocity += ACCELERATION / 60;
+		}
 	}
 	bool getMirrored() {
 		return this->mirrored;
@@ -151,8 +183,8 @@ public:
 	GameWindow(): 
 		Window (WIDTH, HEIGHT),
 		Hintergrund ("Hintergrund.png"),
-		Redcar("Car_red", WIDTH/1.5, HEIGHT - 150),
-		Bluecar("Car_blue", WIDTH/2.5, HEIGHT - 150)
+		Redcar("Car_red", arena, WIDTH/1.5, HEIGHT - 150),
+		Bluecar("Car_blue", arena, WIDTH/2.5, HEIGHT - 150)
 	{
 		set_caption("Gosu Tutorial mit Git");
 	}
@@ -170,20 +202,10 @@ public:
 	void update() override
 	{
 		Redcar.move();
-		if (Gosu::Input::down(Gosu::Button::KB_RIGHT)) {
-			Redcar.accelerate();
-		}
-		if (Gosu::Input::down(Gosu::Button::KB_LEFT)) {
-			Redcar.accelerate(-1);
-		}
-
+		Redcar.accelerate(Gosu::Button::KB_LEFT, Gosu::Button::KB_RIGHT);
+		
 		Bluecar.move();
-		if (Gosu::Input::down(Gosu::Button::KB_D)) {
-			Bluecar.accelerate();
-		}
-		if (Gosu::Input::down(Gosu::Button::KB_A)) {
-			Bluecar.accelerate(-1);
-		}
+		Bluecar.accelerate(Gosu::Button::KB_A, Gosu::Button::KB_D);
 	}
 };
 
